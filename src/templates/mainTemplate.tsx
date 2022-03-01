1134b01205d289ec7fd89a04c69cc0ca97b0b345
KamilGrabowski <grabowskikamil@vp.pl>
2022-03-01 20:37:30 +0100

    added themes and assets

--- src/templates/mainTemplate.tsx
+++ src/templates/mainTemplate.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import GlobalStyle from '../theme/globalStyle'
+import theme from '../theme/theme'
+import { ThemeProvider } from 'styled-components'
 
-export default class MainTemplate extends Component {
+class MainTemplate extends Component {
     render() {
         const { children } = this.props
         return (
-            <>
+            <ThemeProvider theme={theme}>
                 <GlobalStyle />
                 {children}
-            </>
+            </ThemeProvider>
         )
     }
 }
+export default MainTemplate
