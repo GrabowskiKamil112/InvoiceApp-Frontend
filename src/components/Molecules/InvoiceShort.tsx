c0845197a982a4674cce109785028dab27e778e5
Grabowski Kamil <grabowskikamil@vp.pl>
2022-05-04 22:46:59 +0200

    added tempalte for invoiceShort and changed grid resposivenes of invoiceShort

--- src/components/Molecules/InvoiceShort.tsx
+++ src/components/Molecules/InvoiceShort.tsx
@@ -1,80 +1,34 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
-import PageContext from '../../context/pageContext'
-import { calculateTotalOfInvoice, themeNavigator } from '../../utils/utils'
+import React, { useEffect, useState } from 'react'
+import { calculateTotalOfInvoice, getWindowWidth } from '../../utils/utils'
 import Header from '../Atoms/Header'
 import Status from '../Atoms/Status'
 import arrowRight from '../../../public/assets/icon-arrow-right.svg'
 import { Invoice } from '../../Types/Invoice'
 import moment from 'moment'
+import { InvoiceShortTemplate } from '../../templates/InvoiceShortTemplate'
 
-const StyledNavLink = styled(NavLink)<{ themeCtx: string; transitionDelay: number }>`
-    background-color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.invoiceItem.bg`)};
-    grid-template-columns: 80px 144px minmax(auto, 35%) 1fr repeat(2, auto);
-    transition: opacity 400ms ease-out ${({ transitionDelay }) => `${transitionDelay}ms`},
-        outline 0.3s ease;
-    outline: 1px solid rgba(124, 93, 250, 0);
-    align-items: center;
-    border-radius: 8px;
-    padding: 16px 24px;
-    font-size: 1.2rem;
-    color: #ffffff;
-    cursor: pointer;
-    width: 100%;
-    height: 74px;
-    display: grid;
-    gap: 10px;
-
-    &.item-enter {
-        opacity: 0;
-    }
-    &.item-enter-active {
-        opacity: 1;
-    }
-    &.item-exit {
-        opacity: 1;
-    }
-    &.item-exit-active {
-        opacity: 0;
-    }
-
-    & > div {
-        color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.invoiceStatus.text`)};
-        &:nth-child(2),
-        &:nth-child(3) {
-            color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.text.bodyA`)};
-        }
-    }
-    &:hover {
-        outline: 1px solid #7c5dfa;
-    }
-    & h2 {
-        margin-right: 20px;
-    }
-    & h3 {
-        text-transform: uppercase;
-        &::first-letter {
-            color: #7e88c3;
-        }
-    }
-`
 interface Props {
     content: Invoice & { _id: string }
     transitionDelay: number
 }
 const InvoiceShort: React.FC<Props> = ({ content, transitionDelay }) => {
-    const { activeTheme } = useContext(PageContext)
     const { type, _id: id, payment_term, to, items_list } = content
+    const [windowWidth, setWindowWidth] = useState(getWindowWidth())
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(getWindowWidth())
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
     return (
-        <StyledNavLink
-            themeCtx={activeTheme}
-            transitionDelay={transitionDelay}
-            to={`/invoice/${id}`}>
+        <InvoiceShortTemplate transitionDelay={transitionDelay} id={id}>
             <Header size="small">
                 <span>#</span>
-                {id.substring(id.length - 6)}
+                {id?.substring(id.length - 6)}
             </Header>
             <div>Due {moment(payment_term).format('DD MMM YYYY')}</div>
             <div>{to?.name}</div>
@@ -83,8 +37,8 @@ const InvoiceShort: React.FC<Props> = ({ content, transitionDelay }) => {
                 {items_list ? `${calculateTotalOfInvoice(items_list)}` : ''}
             </Header>
             <Status type={type}>{type}</Status>
-            <img src={arrowRight} alt="arrowRight" />
-        </StyledNavLink>
+            {windowWidth > 750 && <img src={arrowRight} alt="arrowRight" />}
+        </InvoiceShortTemplate>
     )
 }
 
---
+++ src/templates/InvoiceShortTemplate.tsx
@@ -0,0 +1,98 @@
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import PageContext from '../context/pageContext'
+import { themeNavigator } from '../utils/utils'
+
+const StyledNavLink = styled(NavLink)<{ themeCtx: string; transitionDelay: number }>`
+    background-color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.invoiceItem.bg`)};
+    grid-template-columns: 80px 144px minmax(auto, 35%) 1fr repeat(2, auto);
+    transition: opacity 400ms ease-out ${({ transitionDelay }) => `${transitionDelay}ms`},
+        outline 0.3s ease;
+    outline: 1px solid rgba(124, 93, 250, 0);
+    align-items: center;
+    border-radius: 8px;
+    padding: 16px 24px;
+    font-size: 1.2rem;
+    color: #ffffff;
+    cursor: pointer;
+    width: 100%;
+    height: 74px;
+    display: grid;
+    gap: 10px;
+    @media (max-width: 750px) {
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 50% 1fr 1fr;
+        gap: 0;
+        height: 111px;
+        & > {
+        }
+    }
+
+    &.item-enter {
+        opacity: 0;
+    }
+    &.item-enter-active {
+        opacity: 1;
+    }
+    &.item-exit {
+        opacity: 1;
+    }
+    &.item-exit-active {
+        opacity: 0;
+    }
+
+    & > div {
+        color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.invoiceStatus.text`)};
+
+        &:nth-child(2),
+        &:nth-child(3) {
+            color: ${({ themeCtx }) => themeNavigator(`${themeCtx}.text.bodyA`)};
+        }
+
+        @media (max-width: 750px) {
+            &:nth-of-type(1) {
+                grid-column: 1;
+                align-self: start;
+            }
+            &:nth-of-type(2) {
+                grid-area: 1/2;
+                justify-self: end;
+                align-self: start;
+            }
+        }
+    }
+    &:hover {
+        outline: 1px solid #7c5dfa;
+    }
+    & h2 {
+        margin-right: 20px;
+    }
+    & > header {
+        @media (max-width: 750px) {
+            align-self: start;
+        }
+
+        & h3 {
+            text-transform: uppercase;
+            &::first-letter {
+                color: #7e88c3;
+            }
+        }
+    }
+`
+type props = {
+    id: string
+    transitionDelay: number
+}
+export const InvoiceShortTemplate: React.FC<props> = ({ children, id, transitionDelay }) => {
+    const { activeTheme } = useContext(PageContext)
+    return (
+        <StyledNavLink
+            transitionDelay={transitionDelay}
+            themeCtx={activeTheme}
+            to={`/invoice/${id}`}>
+            {children}
+        </StyledNavLink>
+    )
+}
